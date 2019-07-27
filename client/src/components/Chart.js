dabf5a50be7f98b5043ebe63b1bd6ce7c99dac98
Tony Choi <tchoi52@gmail.com>
2019-07-27 00:34:07 -0400

    Separate vertical graph frames

--- client/src/components/Chart.js
+++ client/src/components/Chart.js
@@ -1,28 +1,67 @@
 import React, { Component } from 'react';
-import { HorizontalBar } from 'react-chartjs-2';
+import Chart from "chart.js";
 import "./Charts.css";
-const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-        {
-            label: 'My First dataset',
-            backgroundColor: 'rgba(255,99,132,0.2)',
-            borderColor: 'rgba(255,99,132,1)',
-            borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
-            data: [65, 59, 80, 81, 56, 55, 40]
-        }
-    ]
-};
-class Chart extends Component {
 
+class Charts extends Component {
+    dispatchChart = React.createRef();
+    counselChart = React.createRef();
+    
     componentDidMount() {
         console.log("inside chart.js");
         fetch('/api/get-cad-data')
             .then(results => results.text())
             .then(text => console.log(text));
+
+        //DISPATCH CHART
+        const myDispatchChart = this.dispatchChart.current.getContext("2d");
+        
+        new Chart(myDispatchChart, {
+            type: "bar",
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [
+                    {
+                        label: 'All Calls',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: [65, 59, 80, 81, 56, 55, 40, 44, 32, 90, 72, 13, 3, 69]
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
+
+
+        //COUNSEL CHART
+        const myCounselChart = this.counselChart.current.getContext("2d");
+        
+        new Chart(myCounselChart, {
+            type: "bar",
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+                datasets: [
+                    {
+                        label: 'All Calls',
+                        backgroundColor: 'rgba(255,99,132,0.2)',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: [23, 78, 45, 86, 21, 44, 77, 120, 44, 66, 99, 35, 59, 42]
+                    }
+                ]
+            },
+            options: {
+                //Customize chart options
+            }
+        });
     }
+
 
     render() {
         return (
@@ -32,7 +71,10 @@ class Chart extends Component {
                         <div className="card dispatchCard">
                             <div className="card-body">
                                 <h5 class="card-title">Dispatch</h5>
-                                <HorizontalBar data={data} />
+                                <canvas
+                                    id="myChart"
+                                    ref={this.dispatchChart}
+                                />
                             </div>
                         </div>
                     </div>
@@ -40,7 +82,10 @@ class Chart extends Component {
                         <div className="card counselCard">
                             <div className="card-body">
                                 <h5 class="card-title">Counsel</h5>
-                                <HorizontalBar data={data} />
+                                <canvas
+                                    id="myChart"
+                                    ref={this.counselChart}
+                                />
                             </div>
                         </div>
                     </div>
@@ -50,4 +95,4 @@ class Chart extends Component {
     }
 }
 
-export default Chart;
+export default Charts;
