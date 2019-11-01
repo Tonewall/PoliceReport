86078ed247b0c762308ad9c85ed4719511f31bad
tommyyun96 <40390211+tommyyun96@users.noreply.github.com>
2019-11-01 14:21:10 -0400

    pretty table

--- client/src/components/DirectQuerying/DirectQuery.js
+++ client/src/components/DirectQuerying/DirectQuery.js
@@ -1,11 +1,38 @@
 import React, { Component } from "react";
+import { MDBDataTable, MDBBtn } from 'mdbreact';
 import './DirectQuery.css'
 
 export default class DirectQuery extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {user_query: "", query_result:""}
+        this.state = {user_query: "", query_result:[]}
+    }
+    
+    render() {
+        return (
+            <div className="direct_query_main_wrapper">
+                <textarea className="query_box" value = {this.state.user_query} onKeyDown = {this.handle_query_box_key_down}
+                            placeholder="SELECT TOP (1000) [CaseId], [Date], [Time], [Location], [Officer], [Description] FROM [Crime_Report]"
+                            onChange={(event)=>{this.setState({user_query: event.target.value});}}/>
+                <div style={{ textAlign: "right" }}>
+                    <button className="query_button" onClick={this.run_query}>Run query!</button>
+                </div>
+                {/*<div className="query_result" >{this.state.query_result}</div>*/}
+                <div className= "query_result">
+                    <MDBDataTable
+                        scrollX
+                        scrollY
+                        striped
+                        bordered
+                        hover
+                        maxHeight="40vh"
+                        entries={20}
+                        data={this.state.query_result}
+                    />
+                </div>
+            </div>
+        );
     }
 
     handle_query_box_key_down = (event) => {
@@ -26,22 +53,19 @@ export default class DirectQuery extends Component {
     run_query = () => {
         fetch('/direct-query/'+this.state.user_query)
             .then(result => result.json().then(data=> {
-                console.log(data)
-                this.setState({query_result: JSON.stringify(data)});
+                {/*this.setState({query_result: JSON.stringify(data)});*/}
+                this.populateData(data)
             }))
     }
 
-    render() {
-        return (
-            <div className="direct_query_main_wrapper">
-                <textarea className="query_box" value = {this.state.user_query} onKeyDown = {this.handle_query_box_key_down}
-                            placeholder="SELECT TOP (1000) [CaseId], [Date], [Time], [Location], [Officer], [Description] FROM [Crime_Report]"
-                            onChange={(event)=>{this.setState({user_query: event.target.value});}}/>
-                <div style={{ textAlign: "right" }}>
-                    <button className="query_button" onClick={this.run_query}>Run query!</button>
-                </div>
-                <div className="query_result" >{this.state.query_result}</div>
-            </div>
-        );
+    populateData = function (data) {
+        this.setState({
+            query_result: {
+                columns: Object.keys(data[0]).map((key)=>{ return {label: key, field: key, sort: key, width: 200}}),
+                rows: data
+            }
+        }
+        )
     }
+
 }
