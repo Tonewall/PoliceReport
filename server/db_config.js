dcf235dd49c6fd7345038788e57579e77e5026e3
tommy <tommyyun96@gmail.com>
2019-11-12 00:35:31 -0500

    Create db_config.js

---
+++ server/db_config.js
@@ -0,0 +1,20 @@
+module.exports = 
+{
+  server: '130.207.68.107',
+  database: 'CrimeAnalytics',
+  Trusted_Connection: 'Yes',
+  Driver: '{SQL Server Native Client 11.0}',
+  domain: 'AD'
+}
+
+
+/*
+exports.config = 
+{
+  database: "CrimeAnalytics",
+  server: "gtpd-curie",
+  options: {
+    trustedConnection: true
+  }
+};
+*/
