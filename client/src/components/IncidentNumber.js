10ad4b3e220924795b5357e2aa93d469a3fa7a68
Tony Choi <tchoi52@gmail.com>
2019-11-18 14:45:17 -0500

    incident number search and fetch incident

--- client/src/components/IncidentNumber.js
+++ client/src/components/IncidentNumber.js
@@ -3,18 +3,28 @@ import './IncidentNumber.css';
 
 class incidentNumber extends Component {
 
+
+    handleLoginChange(typed) {
+        var link = '/full-report/' + typed;
+        document.getElementById("link").setAttribute("href",link)
+    }
+
     render() {
         return(
         <div className="main">
             <div className="card IncidentNumberCard">
                 <h2 className="card-header">Search by Incident Number</h2>
                 <div className="card-body">
-                    <input type="text" className="form-control" id="inputIncidentNumber" placeholder="Incident Number"/>
+                    <input type="text" className="form-control" onChange={e => this.handleLoginChange(e.target.value)}placeholder="Incident Number"/>
                 </div>
                 <div className="row">
                     <div className="col-12">
                         <div className="searchButton">
-                            <button type="submit" className="btn btn-primary">Search</button>
+                            <a id="link" href="/full-report/0000000000">
+                            <button className="btn btn-primary">
+                                Search
+                            </button>
+                            </a>
                         </div>
                     </div>
                 </div>
