80e9022706938afbcb8c76fb3a51f5e0a85812fd
Tony Choi <tchoi52@gmail.com>
2020-02-15 15:25:39 -0500

    possible fix, field is pointless

--- client/src/components/CommonLibrary.js
+++ client/src/components/CommonLibrary.js
@@ -18,14 +18,15 @@ for(var i=0;i<incident_keys.length;i++)
 
 module.exports.incident_datatable_feeds = function(data)
 {
-    data.forEach(element => {
+    console.log(data)
+    // data.forEach(element => {
         
-        incident_keys.forEach(key => {
-            if(element[key] == null)    element[key]='-'
-            else if(element[key] === true) element[key]='true'
-            else if(element[key] === false)    element[key]='false'
-        });
-    });
+    //     incident_keys.forEach(key => {
+    //         if(element[key] == null)    element[key]='-'
+    //         else if(element[key] === true) element[key]='true'
+    //         else if(element[key] === false)    element[key]='false'
+    //     });
+    // });
 
     return {'rows': data, 'columns': incident_columns}
 }
--- client/src/components/Data.js
+++ client/src/components/Data.js
@@ -20,17 +20,39 @@ class Data extends Component {
     populateData = function (data) {
         /* Need to preprocess query result before */
         var datatable_feeds = incident_datatable_feeds(data)
+        var rows = [];
         for(var i = 0; i < datatable_feeds['rows'].length; i++) {
             var incidentNumber = datatable_feeds['rows'][i]['Incident Number']
             var link = "./full-report/"+incidentNumber
             datatable_feeds['rows'][i]['Incident Number'] = <Link to={link}>{incidentNumber}</Link>
+            var row = {
+                1: <Link to={link}>{incidentNumber}</Link>,
+                Offense: datatable_feeds['rows'][i]['Offense'],
+                Department: datatable_feeds['rows'][i]['Department'],
+            }
+            rows.push(row)
         }
+        var columns = [
+            {label: "Incident Number", field: "Incident Number", width: 100, sort: 'asc'},
+            {label: "Offense", field: "Offense", width: 200, sort: 'asc'},
+            {label: "Department", field: "Department", width: 100, sort: 'asc'},
+        ]
+    //     'Incident Number', 
+    // 'Report Date', 
+    // 'Time', 
+    // 'Offense', 
+    // 'Case Status',
+    // 'Location',
+    // 'Location Landmark', 
+    // 'Offender Name', 
+    // 'Officer Name', 
+    // 'Department', 
         this.setState({
             no_history: false,
             wrong_query: false,
             crimeData: {
-                columns: datatable_feeds['columns'],
-                rows: datatable_feeds['rows']
+                columns: columns,
+                rows: rows
             }
         })
     }
