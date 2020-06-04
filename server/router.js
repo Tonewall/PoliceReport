3c64f5444a8e77a6ec26b9b4fdaa17754d928771
yun.sj <tommyyun96@gmail.com>
2020-06-05 00:50:12 +0900

    configure router for CAS

--- server/router.js
+++ server/router.js
@@ -4,6 +4,9 @@ var config = require('./db_config')
 const read = require('read')
 const { exec } = require('child_process')
 const sprintf = require('sprintf-js').sprintf;
+const https = require('https')
+const parseString = require('xml2js').parseString;
+const dateTime = require('node-datetime')
 
 
 // Contains methods for generating common query.
@@ -46,6 +49,106 @@ function db_query(query_string, next) {
 
 // Router
 function add_router(app) {
+
+
+    var session_checker = function (req, res, next) {
+        if (req.originalUrl == '/verify_user' || req.originalUrl == '/validate_ticket') {
+            // if it's user verifyting or ticket validating, pass.
+            next()
+        }
+        else {
+            var sess = req.session
+            if (!sess.authorized) {
+                // if it's db access but the user is not authorized, deny.
+                console.log('Illegal direct access to database. ip: ' + req.ip)
+                res.send('Not authorized.')
+            }
+            else {
+                // good to go.
+                next();
+            }
+        }
+    }
+
+    app.use(session_checker)
+
+    app.get('/verify_user', function (req, res) {
+        var sess = req.session
+        if (sess.authorized) {
+            res.json({ authorized: true, logged_in: true })
+        }
+        else if (!sess.username) {
+            // no log in info in the session
+            res.json({ authorized: false, logged_in: false })
+        }
+        else {
+            res.json({ authorized: false, logged_in: true })
+        }
+    })
+
+
+    app.post('/validate_ticket', function (req, res) {
+        var sess = req.session
+
+        loginHost = 'login.gatech.edu'
+
+        reqBody = ' <SOAP-ENV:Envelope>\
+                  <SOAP-ENV:Header/>\
+                  <SOAP-ENV:Body>\
+                    <samlp:Request>\
+                      <samlp:AssertionArtifact>' + req.body.ticket + '</samlp:AssertionArtifact>\
+                    </samlp:Request>\
+                  </SOAP-ENV:Body>\
+                </SOAP-ENV:Envelope>'
+
+        const options = {
+            hostname: loginHost,
+            path: '/cas/samlValidate?TARGET=https%3A%2F%2Fgtpd-crimeanalysis.police.gatech.edu',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/xml',
+                'Content-Length': reqBody.length
+            }
+        }
+
+        var samlReq = https.request(options, function (validateResponse) {
+            var body = '';
+            validateResponse.on('data', function (chunk) {
+                body += chunk;
+            });
+            validateResponse.on('end', function () {
+                //handling the response
+                parseString(body, function (err, result) {
+                    try {
+                        logged_in_user = result['SOAP-ENV:Envelope']
+                        ['SOAP-ENV:Body'][0]
+                        ['saml1p:Response'][0]
+                        ['saml1:Assertion'][0]
+                        ['saml1:AttributeStatement'][0]
+                        ['saml1:Subject'][0]
+                        ['saml1:NameIdentifier'][0]
+                        sess.username = logged_in_user
+                        if (body.indexOf('gtpd-crimeanalytics') != -1) {
+                            sess.authorized = true
+                            res.json({ success: true })
+                            console.log('Access attempt by ' + logged_in_user + ' at ' + dateTime.create().format('Y-m-d H:M:S') + ': success')
+                        }
+                        else {
+                            sess.authorized = false
+                            res.json({ success: false, login_failed: false })
+                            console.log('Access attempt by ' + logged_in_user + ' at ' + dateTime.create().format('Y-m-d H:M:S') + ': fail(not authorized)')
+                        }
+                    }
+                    catch (e) {
+                        res.json({ success: false, login_failed: true })
+                        console.log('Access attempt by ' + logged_in_user + ' at ' + dateTime.create().format('Y-m-d H:M:S') + ': fail(ticket validation error')
+                    }
+                });
+            });
+        });
+        samlReq.write(reqBody)
+    })
+
     app.get('/showall', function (req, res) {
         queryString = query_factory.showall();
         db_query(queryString, (err, result) => {
@@ -131,8 +234,7 @@ function add_router(app) {
     app.get('/crimeCategories', function (req, res) {
         db_query(query_factory.crimeCategories, (err, result) => {
             if (!err) res.send(result);
-            else 
-            {
+            else {
                 console.log(err)
                 res.status(400).send(err);
             }
@@ -195,7 +297,7 @@ function add_router(app) {
         });
     });
 
-    
+
     /*
         Integrates basic_info with offense_desc, narratives, offender_info, arrest_info, property_info
         Usage example: 
@@ -206,10 +308,8 @@ function add_router(app) {
             - Arrest Information: incident_info['Arrest Info'] (array)
             - Property Information: incident_info['Property Info'] (array)
     */
-    app.get('/incident-number-integrated/:incident_number', function(req, res)
-    {
-        (async ()=> 
-        {
+    app.get('/incident-number-integrated/:incident_number', function (req, res) {
+        (async () => {
             // resolve basic info
             basic_info_resolver = new Promise(async (res, rej) => {
                 query = query_factory.get_incident_basic(req.params.incident_number)
@@ -223,8 +323,8 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info = await basic_info_resolver.catch(err=>res.status(400).send(err))
-            if(incident_info == null)   return
+            incident_info = await basic_info_resolver.catch(err => res.status(400).send(err))
+            if (incident_info == null) return
 
             // resolve MO
             MO_resolver = new Promise(async (res, rej) => {
@@ -239,8 +339,8 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info['MO'] = await MO_resolver.catch(err=>console.log('Couldn\'t retrieve MO, but will not throw an error.'))
-            
+            incident_info['MO'] = await MO_resolver.catch(err => console.log('Couldn\'t retrieve MO, but will not throw an error.'))
+
             // resolve offense_desc
             offense_desc_resolver = new Promise(async (res, rej) => {
                 query = query_factory.get_offense_description(req.params.incident_number)
@@ -254,8 +354,8 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info['Offense Description'] = await offense_desc_resolver.catch(err=>console.log('Couldn\'t retrieve offense description, but will not throw an error.'))
-            
+            incident_info['Offense Description'] = await offense_desc_resolver.catch(err => console.log('Couldn\'t retrieve offense description, but will not throw an error.'))
+
             // resolve narratives
             narratives_resolver = new Promise(async (res, rej) => {
                 query = query_factory.get_narrative_GTPD(req.params.incident_number)
@@ -269,7 +369,7 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info['Narratives'] = await narratives_resolver.catch(err=>console.log('Couldn\'t retrieve narratives, but will not throw an error.'))
+            incident_info['Narratives'] = await narratives_resolver.catch(err => console.log('Couldn\'t retrieve narratives, but will not throw an error.'))
 
             // resolve offender_info
             offender_info_resolver = new Promise(async (res, rej) => {
@@ -284,7 +384,7 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info['Offender Info'] = await offender_info_resolver.catch(err=>console.log('Couldn\'t retrieve offender information, but will not throw an error.'))
+            incident_info['Offender Info'] = await offender_info_resolver.catch(err => console.log('Couldn\'t retrieve offender information, but will not throw an error.'))
 
             // resolve arrest_info
             arrest_info_resolver = new Promise(async (res, rej) => {
@@ -299,7 +399,7 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info['Arrest Info'] = await arrest_info_resolver.catch(err=>console.log('Couldn\'t retrieve arrest information, but will not throw an error.'))
+            incident_info['Arrest Info'] = await arrest_info_resolver.catch(err => console.log('Couldn\'t retrieve arrest information, but will not throw an error.'))
 
             // resolve property_info
             property_info_resolver = new Promise(async (res, rej) => {
@@ -314,7 +414,7 @@ function add_router(app) {
                     else rej(err);
                 });
             });
-            incident_info['Property Info'] = await property_info_resolver.catch(err=>console.log('Couldn\'t retrieve property information, but will not throw an error.'))
+            incident_info['Property Info'] = await property_info_resolver.catch(err => console.log('Couldn\'t retrieve property information, but will not throw an error.'))
 
             res.send(incident_info);
         })();
@@ -363,8 +463,7 @@ function add_router(app) {
         query = query_factory.get_narrative_APD(req.params.incident_number)
         db_query(query, (err, result) => {
             if (!err) {
-                if (result != null && result[0] != null)
-                {
+                if (result != null && result[0] != null) {
                     res.send(result[0]);
                 }
                 else
@@ -377,8 +476,7 @@ function add_router(app) {
         query = query_factory.get_offender(req.params.incident_number)
         db_query(query, (err, result) => {
             if (!err) {
-                if (result != null)
-                {
+                if (result != null) {
                     res.send(result);
                 }
                 else
@@ -391,8 +489,7 @@ function add_router(app) {
         query = query_factory.get_victim(req.params.incident_number)
         db_query(query, (err, result) => {
             if (!err) {
-                if (result != null)
-                {
+                if (result != null) {
                     res.send(result);
                 }
                 else
@@ -405,8 +502,7 @@ function add_router(app) {
         query = query_factory.get_complainant(req.params.incident_number)
         db_query(query, (err, result) => {
             if (!err) {
-                if (result != null)
-                {
+                if (result != null) {
                     res.send(result);
                 }
                 else
@@ -420,8 +516,7 @@ function add_router(app) {
         query = query_factory.get_narrative_GTPD(req.params.incident_number)
         db_query(query, (err, result) => {
             if (!err) {
-                if (result != null && result[0] != null)
-                {
+                if (result != null && result[0] != null) {
                     res.send(result[0]);
                 }
                 else
