590e6397ca4a2ab3ee85dd0c958178c935067a70
yun.sj <tommyyun96@gmail.com>
2020-06-08 10:19:29 +0900

    build file for deployment

---
+++ client/build/precache-manifest.ae439dbac8fd6d17f7c5c5a1dbd026e1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49a935243565d84e154854892b2f8b39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0c6e33ac8c8722b5b637",
+    "url": "/static/css/2.53240637.chunk.css"
+  },
+  {
+    "revision": "497b48cb335c9f4a869f",
+    "url": "/static/css/main.e58d0a30.chunk.css"
+  },
+  {
+    "revision": "0c6e33ac8c8722b5b637",
+    "url": "/static/js/2.0c8cb7b1.chunk.js"
+  },
+  {
+    "revision": "497b48cb335c9f4a869f",
+    "url": "/static/js/main.26537e86.chunk.js"
+  },
+  {
+    "revision": "875d570ce1b6fc43afc6",
+    "url": "/static/js/runtime-main.e077b804.js"
+  }
+]);
